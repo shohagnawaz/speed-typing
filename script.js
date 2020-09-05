e35b6cadf79d41fd1463173b3bd420268aeeaa96
Shohag Nawaz <snawazdigital@gmail.com>
2020-09-05 15:41:42 +0600

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const quoteInputElement = document.getElementById('quoteInput');
 const timerElement = document.getElementById('timer');
 
 
-quoteInputElement.addEventListener('click', () => {
+quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span');
     const arrayValue = quoteInputElement.value.split('');
 
@@ -15,12 +15,12 @@ quoteInputElement.addEventListener('click', () => {
         if (character == null) {
             characterSpan.classList.remove("correct");
             characterSpan.classList.remove("incorrect");
-            correct = false;
-        }
+            correct = false; 
+        } 
         else if (character === characterSpan.innerText) {
             characterSpan.classList.add("correct");
             characterSpan.classList.remove("incorrect");
-        }
+        } 
         else {
             characterSpan.classList.remove("incorrect");
             characterSpan.classList.add("correct");
@@ -40,10 +40,9 @@ function getRandomQuote() {
 
 async function renderNewQuote() {
     const quote = await getRandomQuote()
-    quoteDisplayElement.innerHTML = quote;
+    quoteDisplayElement.innerHTML = '';
     quote.split('').forEach(character => {
         const characterSpan = document.createElement('span');
-        // characterSpan.classList.add('incorrect');
         characterSpan.innerText = character;
         quoteDisplayElement.appendChild(characterSpan)
     });
@@ -51,7 +50,7 @@ async function renderNewQuote() {
     startTimer();
 }
 
-let startTime
+let startTime;
 function startTimer() {
     timerElement.innerText = 0;
     startTime = new Date();
