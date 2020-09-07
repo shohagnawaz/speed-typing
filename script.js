0f7f1c20f60cc7525d5559b9d0cbf18382a27b80
Shohag Nawaz <snawazdigital@gmail.com>
2020-09-07 10:12:40 +0600

    finished

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
-//Selector
-const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+//Constant Variable
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteInput');
 const timerElement = document.getElementById('timer');
 
-
-quoteInputElement.addEventListener('input', () => {
-    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
-    const arrayValue = quoteInputElement.value.split('');
-
-    let correct = true;
-    arrayQuote.forEach((characterSpan, index) => {
-        const character = arrayValue[index];
-        if (character == null) {
-            characterSpan.classList.remove("correct");
-            characterSpan.classList.remove("incorrect");
-            correct = false; 
-        } 
-        else if (character === characterSpan.innerText) {
-            characterSpan.classList.add("correct");
-            characterSpan.classList.remove("incorrect");
-        } 
-        else {
-            characterSpan.classList.remove("incorrect");
-            characterSpan.classList.add("correct");
-            correct = false;
-        }
-    });
-
-    if (correct) renderNewQuote;
-});
-
 //Function
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
-        .then(response => response.json())
-        .then(data => data.content)
+            .then(response => response.json())
+            .then(data => data.content)
 }
 
 async function renderNewQuote() {
-    const quote = await getRandomQuote()
+    const quote = await getRandomQuote();
     quoteDisplayElement.innerHTML = '';
     quote.split('').forEach(character => {
         const characterSpan = document.createElement('span');
@@ -50,17 +23,45 @@ async function renderNewQuote() {
     startTimer();
 }
 
+renderNewQuote()
+
+//EventListener
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+    const arrayValue = quoteInputElement.value.split('');
+
+    let correct = true;
+    arrayQuote.forEach((characterSpan, index) => { 
+        const character = arrayValue[index];
+        if (character == null) {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+            correct = false;
+        }
+        else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect')
+        }
+        else {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+            correct = false;
+        }
+    });
+    
+    if (correct) renderNewQuote();
+});
+
+//Timer Function
 let startTime;
 function startTimer() {
     timerElement.innerText = 0;
-    startTime = new Date();
+    startTime = new Date()
     setInterval(() => {
         timer.innerText = getTimerTime()
-    }, 1000)
+    }, 1000);
 }
 
 function getTimerTime() {
-    return Math.floor((new Date() - startTime) / 1000)
+    return Math.floor((new Date() - startTime) / 1000);
 }
-
-renderNewQuote()
